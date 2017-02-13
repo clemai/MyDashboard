f9ce0afe37480a48220eb52e246eda83ff38f097
Fatboyslimc <clemens_maier@gmx.de>
2017-02-13 23:01:22 +0100

    Added weather forecast data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 import settings from './settings.json';
 
+
+
+function WeatherForceast(oWeatherData) {
+	const oList = oWeatherData.list;
+	var mGroupbyDate = [];
+	oList.map((oItem) => {
+		var sDate = new Date(oItem.dt_txt).toDateString();
+		if (mGroupbyDate[sDate]) {
+			mGroupbyDate[sDate].push(oItem);
+		} else {
+			mGroupbyDate[sDate] = [oItem];
+		}
+	}
+	);
+	var oRenderList = [];
+	Object.keys(mGroupbyDate).forEach((sKey) => {
+		oRenderList.push(<li key={mGroupbyDate[sKey][0].dt_txt} > {
+			new Date(mGroupbyDate[sKey][0].dt_txt).toLocaleString(settings.locale, { weekday: 'long' })
+		}: {
+				mGroupbyDate[sKey].map((oItem) => {
+					return oItem.main.temp + "°C";
+				}).join(", ")}
+		</li>)
+	});
+	return (<ul> {oRenderList} </ul>);
+}
+
+
+
 class App extends React.Component {
 
 	constructor(props) {
@@ -12,18 +41,16 @@ class App extends React.Component {
 			football: {}
 		};
 	}
-	
-
 	componentDidMount() {
-	// TODO: fetch polyfill https://github.com/github/fetch
+		// TODO: fetch polyfill https://github.com/github/fetch
 		// TODO: Limit API calls
-		fetch('http://api.openweathermap.org/data/2.5/forecast?units=metric&id=2907911&appid=' + settings.openweather.id)
+		fetch('http://api.openweathermap.org/data/2.5/forecast?units=metric&id=' + settings.openweather.cityId + '&appid=' + settings.openweather.id)
 			.then((response) => { return response.json() })
 			.then((oWeatherData) => {
 				this.setState({ weather: oWeatherData })
 			});
 
-		fetch('https://www.openligadb.de/api/getnextmatchbyleagueteam/'+ settings.openliga.season +'/'+ settings.openliga.team)
+		fetch('https://www.openligadb.de/api/getnextmatchbyleagueteam/' + settings.openliga.season + '/' + settings.openliga.team)
 			.then((oResponse) => { return oResponse.json() })
 			.then((oNextGameData) => {
 				this.setState({
@@ -31,6 +58,7 @@ class App extends React.Component {
 				})
 			});
 	}
+
 
 
 	render() {
@@ -42,18 +70,22 @@ class App extends React.Component {
 					<h2>My App</h2>
 				</div>
 				<p className="App-intro"></p>
-					<b>Last Update:</b> {new Date().toString()} <br></br>
-					<hr></hr>
-					<b>Heidelberg weather:</b> {this.state.weather && this.state.weather.list && this.state.weather.list.main ? this.state.weather.list.main.temp + "°C": "Request failed"} <br></br>
-					<hr></hr>
-					<b>Next football game: </b>
-					{
-						this.state.football && this.state.football.Team1 && this.state.football.Team2 &&
-						<div>
-							<img src={this.state.football.Team1.TeamIconUrl}></img>{this.state.football.Team1.TeamName} vs.
+				<b>Last Update:</b> {new Date().toString()} <br></br>
+				<hr></hr>
+				<b>Heidelberg weather:</b>
+				{
+					this.state.weather && this.state.weather.list && this.state.weather.list.length > 0 ? <WeatherForceast list={this.state.weather.list} /> : "Request failed"
+				}
+				<br></br>
+				<hr></hr>
+				<b>Next football game: </b>
+				{
+					this.state.football && this.state.football.Team1 && this.state.football.Team2 &&
+					<div>
+						<img src={this.state.football.Team1.TeamIconUrl}></img>{this.state.football.Team1.TeamName} vs.
 							<img src={this.state.football.Team2.TeamIconUrl}></img>{this.state.football.Team2.TeamName} on {this.state.football.MatchDateTime}
-						</div>
-					}
+					</div>
+				}
 			</div>
 		);
 	}
